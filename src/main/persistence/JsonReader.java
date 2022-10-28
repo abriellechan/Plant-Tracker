e313d020bd69e2e88546614e11f7bafa49bdf7c9
abrielle <abriellechan@gmail.com>
2022-10-28 16:09:28 -0700

    added library to kid

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -22,7 +22,7 @@ public class JsonReader {
         this.source = source;
     }
 
-    // EFFECTS: reads workroom from file and returns it;
+    // EFFECTS: reads gardenlist from file and returns it;
     // throws IOException if an error occurs reading data from file
     public GardenList read() throws IOException {
         String jsonData = readFile(source);
